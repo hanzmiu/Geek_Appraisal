ec8fcb9b48da1764e4167f3f2b510bd7aebb29ba
hanzmiu <2971740632@qq.com>
2022-10-24 23:23:19 +0800

    Modify main.c

--- task1/main.c
+++ task1/main.c
@@ -2,7 +2,30 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-	printf("Hello World!\n");
+	int n = 0;
+	double x = 0;
+	double a = 0;
+	double h = 0.001;
+
+	printf("«Î ‰»Îa:");
+	scanf("%lf", &a);
+
+	double y = a;
+	printf("x\t         y\n");
+	printf("%d\t         %f\n", x, y);
+
+	for (n = 0;  ;) {
+		double Y = y;
+		y = y + h * (-y);
+		x = x + h;
+		n++;
+		printf("%f\t %f\n", x, y);
+		if (Y - y < 0.001 * h) {
+			break;
+		}
+	}
+
 	return 0;
+
 }
 
